689299bb1a90bc6d6bce44e8b573af5e56a5b60a
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-17 16:21:53 -0400

    Datatypes and Variables

--- datatypes.js
+++ datatypes.js
@@ -19,3 +19,30 @@ console.log(str)
 console.log(str.length) // logging the length of the string
 console.log(str.split("")) // splitting the string into an array of individual lettrs
 console.log(str.charAt(5))// show me the fifth character of the string
+
+//Numbers
+// integers(whole numbers), floats(decimals)
+console.log(5)
+let num =5
+console.log(num)
+console.log(num + 5) // 5 + 5 = "10"
+console.log(num + "5") // "5" + "5" = "55" (Type Coercion)
+
+console.log(5 / "cheese") //NaN 
+
+// VARIABLES
+// VARIABLES ARE CONTAINERS FOR VALUES
+
+// const - block scoped, cannot be reassigned
+const bread1 = "sourdough"
+console.log(bread1)
+
+// let - block scoped, can be reassigned
+let bread2 = "wheat"
+bread2 = "white"
+console.log(bread2)
+
+//var - function scoped, can be reassigned
+var bread3 = "white"
+bread3 = "wheat"
+console.log(bread3)
