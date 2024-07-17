e2aad05b40605c3da874602cd651160dd720e8bd
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-17 16:03:02 -0400

    first commit

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+//************************* */
+// DATA TYPES
+//************************* */
+
+
+//Strings
+//One or more characters inside of quotes
+// "",'', ``
+console.log("Hello World")
+console.log('Hello World')
+console.log(`Hello World`)
+
+// declared a variable, stored a string inside of it
+let str= "Hello World"
+console.log(str)
+
+
+// using string properties and methods
+console.log(str.length) // logging the length of the string
+console.log(str.split("")) // splitting the string into an array of individual lettrs
+console.log(str.charAt(5))// show me the fifth character of the string
