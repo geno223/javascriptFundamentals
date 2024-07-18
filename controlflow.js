6a48b73f8fd0f6373b0110c397b97e12d6d946fa
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-18 09:40:59 -0400

    If Statements

---
+++ controlflow.js
@@ -0,0 +1,18 @@
+// --------------------------------------
+// CONTROL FLOW
+// --------------------------------------
+// Define: Control flow is the order in which statements are executed in a program.
+
+// Conditionals
+// If statements which are used to make decisions in code.
+// if (condition) {} else if (condition) {} else {}
+
+let x = 6;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 5) {
+    console.log("x is less than 5");
+} else {
+    console.log("x is equal to 5");
+}
