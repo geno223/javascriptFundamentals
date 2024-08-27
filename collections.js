0084524bcac1b85dba5e5db612f732c1f780a74c
geno223 <eugenio.jimenez22397@gmail.com>
2024-08-27 19:02:40 -0400

    collectons

---
+++ collections.js
@@ -0,0 +1,19 @@
+// ************************
+// COLLECTIONS IN JAVASCRIPT
+// ************************
+// Collections are data structures that hold multiple values.
+
+// Arrays (An ordered list of values)
+// The values are in a specific order
+
+// OLD WAY
+const arr = new Array(1, 2, 3, 4, 5);
+// Current Way
+const arr2 = [1, 2, 3, 4, 5];
+
+// Access the values inside the array using their index
+// indexes always start at 0
+console.log(arr[0], arr2[0])
+console.log(arr[1], arr2[1])
+console.log(arr[2], arr2[2])
+console.log(arr[4], arr2[4])
