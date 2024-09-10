91764fece93b648ec1cfd2a1cb239b5215f0461a
geno223 <eugenio.jimenez22397@gmail.com>
2024-09-10 15:42:47 -0400

    pop quiz

---
+++ popquiz.js
@@ -0,0 +1,8 @@
+// is write a loop that loops 10 times, if the counter variables is equal to 2 or 4, log "I'm special"
+
+
+// write me an if statement that checks that I'm old enough to drink (drinking age 21)
+
+// write me a function that logs my BMI
+// BMI = weight / height^2
+// bmi(weight, height)
